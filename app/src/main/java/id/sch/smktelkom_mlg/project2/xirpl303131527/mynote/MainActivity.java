3c0c8581234b3ea8f99d493d6456acb59f7f1a94
bima <bimacipir@gmail.com>
2017-03-26 16:32:12 +0700

    initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl303131527/mynote/MainActivity.java
@@ -0,0 +1,44 @@
+package id.sch.smktelkom_mlg.project2.xirpl303131527.mynote;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.navigation_home) {
+                fragment = new my();
+            }
+            else if(id == R.id.navigation_dashboard) {
+                fragment = new reminder();
+            }
+            else if(id == R.id.navigation_notifications){
+                fragment = new about();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).commitNow();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
