9fa0db92aa60f3e54907d56f191079c4eeb0a8fc
bima <bimacipir@gmail.com>
2017-03-26 17:33:22 +0700

    add database

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl303131527/mynote/EditActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl303131527/mynote/EditActivity.java
@@ -2,12 +2,71 @@ package id.sch.smktelkom_mlg.project2.xirpl303131527.mynote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import id.sch.smktelkom_mlg.project2.xirpl303131527.mynote.db.DatabaseAccess;
 
-public class EditActivity extends AppCompatActivity {
+public class EditActivity extends ActionBarActivity {
+    private EditText etText;
+    private Button btnSave;
+    private Button btnCancel;
+    private Memo memo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        this.etText = (EditText) findViewById(R.id.etText);
+        this.btnSave = (Button) findViewById(R.id.btnSave);
+        this.btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null) {
+            memo = (Memo) bundle.get("MEMO");
+            if(memo != null) {
+                this.etText.setText(memo.getText());
+            }
+        }
+
+        this.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSaveClicked();
+            }
+        });
+
+        this.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCancelClicked();
+            }
+        });
+    }
+
+    public void onSaveClicked() {
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        if(memo == null) {
+            // Add new memo
+            Memo temp = new Memo();
+            temp.setText(etText.getText().toString());
+            databaseAccess.save(temp);
+        } else {
+            // Update the memo
+            memo.setText(etText.getText().toString());
+            databaseAccess.update(memo);
+        }
+        databaseAccess.close();
+        this.finish();
+    }
+
+    public void onCancelClicked() {
+        this.finish();
     }
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl303131527/mynote/db/DatabaseOpenHelper.java
@@ -0,0 +1,29 @@
+package id.sch.smktelkom_mlg.project2.xirpl303131527.mynote.db;
+
+/**
+ * Created by rawon on 26/03/17.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class DatabaseOpenHelper extends SQLiteOpenHelper {
+    public static final String DATABASE = "memos.db";
+    public static final String TABLE = "memo";
+    public static final int VERSION = 1;
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, DATABASE, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE memo(date INTEGER PRIMARY KEY, memo TEXT);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
