276ab3d24598d9b9fa42ea033b2c1f9cc82f3977
agungwrdn <agungwar00@gmail.com>
2017-03-28 18:04:18 +0700

    combine

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl303131527/mynote/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl303131527/mynote/MainActivity.java
@@ -1,15 +1,32 @@
 package id.sch.smktelkom_mlg.project2.xirpl303131527.mynote;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+import id.sch.smktelkom_mlg.project2.xirpl303131527.mynote.db.DatabaseAccess;
+
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseAccess databaseAccess;
+    private List<Memo> memos;
+    private ListView listView;
+    FloatingActionButton floatingActionButton;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -38,15 +55,108 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).commitNow();
     }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View inflatedView = getLayoutInflater().inflate(R.layout.fragment_my, null);
+        this.databaseAccess = DatabaseAccess.getInstance(this);
+
+        this.listView = (ListView) inflatedView.findViewById(R.id.listView);
+        this.floatingActionButton = (FloatingActionButton) inflatedView.findViewById(R.id.FAB);
+
+        this.floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAddClicked();
+            }
+        });
+
+        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Memo memo = memos.get(position);
+                TextView txtMemo = (TextView) view.findViewById(R.id.txtMemo);
+                if (memo.isFullDisplayed()) {
+                    txtMemo.setText(memo.getShortText());
+                    memo.setFullDisplayed(false);
+                } else {
+                    txtMemo.setText(memo.getText());
+                    memo.setFullDisplayed(true);
+                }
+            }
+        });
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         changePage(R.id.navigation_home);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        databaseAccess.open();
+        this.memos = databaseAccess.getAllMemos();
+        databaseAccess.close();
+        MemoAdapter adapter = new MemoAdapter(this, memos);
+        this.listView.setAdapter(adapter);
+    }
+
+    public void onAddClicked() {
+        Intent intent = new Intent(this, EditActivity.class);
+        startActivity(intent);
+    }
+
+    public void onDeleteClicked(Memo memo) {
+        databaseAccess.open();
+        databaseAccess.delete(memo);
+        databaseAccess.close();
+
+        ArrayAdapter<Memo> adapter = (ArrayAdapter<Memo>) listView.getAdapter();
+        adapter.remove(memo);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void onEditClicked(Memo memo) {
+        Intent intent = new Intent(this, EditActivity.class);
+        intent.putExtra("MEMO", memo);
+        startActivity(intent);
+    }
+
+    private class MemoAdapter extends ArrayAdapter<Memo> {
+
+
+        public MemoAdapter(Context context, List<Memo> objects) {
+            super(context, 0, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.layout_list_item, parent, false);
+            }
+
+            ImageView btnEdit = (ImageView) convertView.findViewById(R.id.btnEdit);
+            ImageView btnDelete = (ImageView) convertView.findViewById(R.id.btnDelete);
+            TextView txtDate = (TextView) convertView.findViewById(R.id.txtDate);
+            TextView txtMemo = (TextView) convertView.findViewById(R.id.txtMemo);
+
+            final Memo memo = memos.get(position);
+            memo.setFullDisplayed(false);
+            txtDate.setText(memo.getDate());
+            txtMemo.setText(memo.getShortText());
+            btnEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onEditClicked(memo);
+                }
+            });
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onDeleteClicked(memo);
+                }
+            });
+            return convertView;
+        }
+    }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl303131527/mynote/my.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl303131527/mynote/my.java
@@ -1,11 +1,23 @@
 package id.sch.smktelkom_mlg.project2.xirpl303131527.mynote;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import id.sch.smktelkom_mlg.project2.xirpl303131527.mynote.db.DatabaseAccess;
 
 
 /**
@@ -23,7 +35,12 @@ public class my extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_my,
+                container, false);
+
+
+        return rootView;
     }
+
 
 }
